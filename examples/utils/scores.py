98feff87c625414cb8dc5912fc76da3d5bfcd288
Ajda Pretnar <ajdapretnar@gmail.com>
2021-01-25 18:58:27 +0100

    Compare methods on Schutz and SemEval

--- examples/utils/scores.py
+++ examples/utils/scores.py
@@ -1,17 +1,17 @@
 def precision(x_pred, x_true):
-    return len([x for x in x_pred if x in x_true]) / len(x_pred)
+    return (len([x for x in x_pred if x in x_true]) / len(x_pred)) if len(x_pred) else 0
 
 
 def recall(x_pred, x_true):
-    return len([x for x in x_pred if x in x_true]) / len(x_true)
+    return (len([x for x in x_pred if x in x_true]) / len(x_true)) if len(x_true) else 0
 
 
 def average_precision(x_pred, x_true):
-    return sum(precision(p, t) for p, t in zip(x_pred, x_true)) / len(x_pred)
+    return (sum(precision(p, t) for p, t in zip(x_pred, x_true)) / len(x_pred)) if len(x_pred) else 0
 
 
 def average_recall(x_pred, x_true):
-    return sum(recall(p, t) for p, t in zip(x_pred, x_true)) / len(x_pred)
+    return (sum(recall(p, t) for p, t in zip(x_pred, x_true)) / len(x_pred)) if len(x_pred) else 0
 
 
 def average_f_score(x_pred, x_true):
@@ -19,7 +19,7 @@ def average_f_score(x_pred, x_true):
     for p, t in zip(x_pred, x_true):
         pr, re = precision(p, t), recall(p, t)
         f_scores.append(2 * pr * re / (pr + re) if (pr + re) else 0)
-    return sum(f_scores) / len(x_pred)
+    return (sum(f_scores) / len(x_pred)) if len(x_pred) else 0
 
 
 def take_n(l, n):
