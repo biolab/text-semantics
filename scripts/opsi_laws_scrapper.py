fd961c472aa017ffb6eeacbeb7287efaf98ff345
Primoz Godec <p.godec9@gmail.com>
2021-09-20 15:37:18 +0200

    Update according to Mihas suggestions

--- scripts/opsi_laws_scrapper.py
+++ scripts/opsi_laws_scrapper.py
@@ -9,8 +9,10 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 # url to source
-FILE_URL = "https://podatki.gov.si/dataset/a989ca8a-08be-4b87-a0ad-a7b6991f387d/" \
-           "resource/b37e18dd-d80e-4327-872e-5f1b355ccc3b/download/vsebina.tar.gz"
+FILE_URL = (
+    "https://podatki.gov.si/dataset/a989ca8a-08be-4b87-a0ad-a7b6991f387d/"
+    "resource/b37e18dd-d80e-4327-872e-5f1b355ccc3b/download/vsebina.tar.gz"
+)
 
 # download dir
 FILE_DIR = "files/"
@@ -25,7 +27,7 @@ RESULTING_FILE_NAME = os.path.join(FILE_DIR, "zakoni-full.csv")
 
 # types to scrape
 # TYPES = {'PRAV', 'ZAKO', 'URED', 'SKLE'}
-TYPES = {'ZAKO'}
+TYPES = {"ZAKO"}
 
 
 def download_laws():
@@ -47,18 +49,31 @@ def unpack_laws():
 def extract_laws():
     """ Extract data from bson file and pickle them """
     print("loading bson")
-    with open(UNPACKED_FILE_NAME, 'rb') as f:
+    with open(UNPACKED_FILE_NAME, "rb") as f:
         data = bson.decode_all(f.read())
     df = pd.DataFrame(data)
-    df = df[df["idPredpisa"].str.contains('|'.join(TYPES))]
-    print("picklking:", len(df))
-    df.to_pickle(PICKLE_NAME)
+    df = df[df["idPredpisa"].str.contains("|".join(TYPES))]
+    df = df.drop(["_id", "cleni", "cleniList"], axis=1)
+    return df
 
 
-def load_pickled_data():
-    """ Load pickled data """
-    print("loading pickle")
-    return pd.read_pickle(PICKLE_NAME)
+def remove_duplicates(df):
+    def replace_with_newer_regulations(s):
+        # find all regulations that change s
+        df_chng = df[df["idPredpisaChng"] == s["idPredpisa"]]
+        # if not existing - return s
+        if len(df_chng) == 0:
+            return s
+        # else replace regulations with newer version -
+        # newer is one that changes s and have highest npb number
+        new_s = df_chng.iloc[df_chng["npbNum"].argmax()]
+        new_s["idPredpisa"] = s["idPredpisa"]
+        return new_s
+
+    # find regulations that are a basis for later changes - those which do not
+    # change any regulation are basis
+    df_orig = df[df["idPredpisaChng"].isnull()]
+    return df_orig.apply(replace_with_newer_regulations, axis=1)
 
 
 # title is contained in html elements with following classes
@@ -76,7 +91,7 @@ def last_title_tag(div):
 
     while True:
         next = start.find_next_sibling()
-        if next.has_attr("class") and len(set(next['class']) & set(title_classes)) > 0:
+        if next.has_attr("class") and len(set(next["class"]) & set(title_classes)) > 0:
             start = next
         else:
             return start
@@ -117,7 +132,6 @@ def print_metadata(row):
 
 skip = [
     ("ZAKO525", 0),  # law is an image
-    ("ZAKO7442", 8),  # specific-format (two columns) # todo: support
 ]
 
 
@@ -133,10 +147,12 @@ def extract_content_columns(df):
             continue
 
         # printing documents to html for visualisation
-        with open(f"files/htmls/{row['idPredpisa']}_{row['npbNum']}.html", "w") as f:
+        with open(
+            f"files/htmls/{row['idPredpisa']}_{row['npbNum']}_{i}.html", "w"
+        ) as f:
             f.write(row["vsebina"])
 
-        soup = BeautifulSoup(row["vsebina"], 'html.parser')
+        soup = BeautifulSoup(row["vsebina"], "html.parser")
         body = soup.body
         divs = body.findAll("div", recursive=False)
         if len(divs) > 1:
@@ -161,7 +177,6 @@ def extract_data(df):
     content = extract_content_columns(df)
     df = df[df.columns.difference(content.columns)]
     df = pd.concat((df, content), axis=1)
-    df = df.drop(["_id", "cleni", "cleniList"], axis=1)
     return df
 
 
@@ -170,10 +185,13 @@ if __name__ == "__main__":
     if not os.path.isfile(PICKLE_NAME):
         download_laws()
         unpack_laws()
-        extract_laws()
+        df = extract_laws()
+        df.to_pickle(PICKLE_NAME)
 
-    df = load_pickled_data()
+    df = pd.read_pickle(PICKLE_NAME)
+    df = remove_duplicates(df)
+    print(len(df))
     df = extract_data(df)
+
     df.to_csv(RESULTING_FILE_NAME, index=False)
     print(f"total time = {time.time() - t} s")
-
